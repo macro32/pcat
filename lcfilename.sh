9c9e1e31d72ebb9b90df0df41fe11d3e8400a1dc
John Ratcliffe <jgratcliffe@btinternet.com>
2019-12-02 20:59:49 +0000

    added some parameter checking.

--- lcfilename.sh
+++ lcfilename.sh
@@ -1,3 +1,29 @@
-#!/usr/bin/bash
-# usage: lcfilename <root_dir>
+#!/bin/bash
+
+helpFunction()
+{
+   echo ""
+   echo "Usage: $0 -r root_dir"
+   echo -e "\t-r Root dir to start depth first search"
+   exit 1 # Exit script after printing help
+}
+
+while getopts "r" opt
+do
+   case "$opt" in
+      r ) parameterA="$OPTARG" ;;
+      ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
+   esac
+done
+
+# Print helpFunction in case parameters are empty
+if [ -z "$parameterA" ]
+then
+   echo "Some or all of the parameters are empty";
+   helpFunction
+fi
+
+# Begin script in case all parameters are correct
+echo "$parameterA"
+
 find $1 -depth -exec rename 's/(.*)\/([^\/]*)/$1\/\L$2/' {} \;
